75b3f466b98148355b42115a4aa3f471d91a921c
Rook <raymondgonzalez22@gmail.com>
2022-06-19 21:47:21 -0400

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,20 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+const { getByText } = render(<App />);
+                             
+test('renders learn react explore', () => {
+  const linkElement = getByText(/explore/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+test('renders learn react login', () => {
+  const linkElement = getByText(/login/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('renders learn react curatedUsers', () => {
+  const linkElement = getByText(/curated users/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
